6300e39214eb3b19c3a61813580c0da67a3e63c8
Sparkybeard <goncalo@bash.pt>
2018-11-21 14:25:21 +0000

    first send

---
+++ ecar-ws/src/main/java/com/ecar/ws/ECarApp.java
@@ -0,0 +1,78 @@
+package com.ecar.ws;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.ecar.domain.ECarManager;
+
+/**
+ * ECarApp
+ * 
+ * Class that registers itself in UDDI and waits for connections Addresses are
+ * passed via pom.xml
+ *
+ */
+public class ECarApp {
+
+	public static void main(String[] args) throws Exception {
+		// Check arguments
+		if (args.length == 0 || args.length == 3) {
+			System.err.println("Argument(s) missing!");
+			System.err.println("Usage: java " + ECarApp.class.getName() + " wsURL OR uddiURL wsName wsURL");
+			return;
+		}
+		String uddiURL = null;
+		String wsName = null;
+		String wsURL = null;
+		boolean primary;
+		String wsi = null;
+
+		// Create server implementation object, according to options
+		ECarEndpointManager endpoint = null;
+		if (args.length == 2) {
+			wsURL = args[0];
+			wsi = args[1];
+			endpoint = new ECarEndpointManager(wsURL, wsi);
+			
+		} else if (args.length >= 4) {
+			uddiURL = args[0];
+			wsName = args[1];
+			wsURL = args[2];
+			wsi = args[3];
+			
+			endpoint = new ECarEndpointManager(uddiURL, wsName, wsURL, wsi);
+			endpoint.setVerbose(true);
+		}
+
+		ECarManager.getInstance().initUddiURL(uddiURL);
+
+		// load ecar properties
+		String parkName = null;
+		try {
+			InputStream inputStream = ECarApp.class.getResourceAsStream("/ecar.properties");
+
+			Properties properties = new Properties();
+			properties.load(inputStream);
+
+			System.out.println("Loaded properties:");
+			System.out.println(properties);
+
+			parkName = properties.getProperty("park.ws.name");
+
+			ECarManager.getInstance().initParkTemplateName(parkName);
+
+		} catch (IOException e) {
+			System.out.printf("Failed to load configuration: %s%n", e);
+		}
+
+		try {
+			endpoint.start();
+			endpoint.awaitConnections();
+		} finally {
+			endpoint.stop();
+		}
+
+	}
+
+}
